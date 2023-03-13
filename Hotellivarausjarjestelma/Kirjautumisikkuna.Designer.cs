2b6c424f46b7984cd0491b27d3ef8402c13be92d
Petja Penttinen <petja.penttinen@edu.keuda.fi>
2023-03-13 12:08:45 +0200

    ööö

--- Hotellivarausjarjestelma/Kirjautumisikkuna.Designer.cs
+++ Hotellivarausjarjestelma/Kirjautumisikkuna.Designer.cs
@@ -32,11 +32,11 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.kayttajaTunnusTB = new System.Windows.Forms.TextBox();
-            this.salaSanaTB = new System.Windows.Forms.TextBox();
             this.KirjauduBT = new System.Windows.Forms.Button();
+            this.salaSanaTB = new System.Windows.Forms.TextBox();
+            this.kayttajaTunnusTB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -44,9 +44,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(282, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(188, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(201, 194);
+            this.pictureBox1.Size = new System.Drawing.Size(134, 126);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -54,16 +55,18 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.LightYellow;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 227);
+            this.panel1.Location = new System.Drawing.Point(8, 148);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(780, 74);
+            this.panel1.Size = new System.Drawing.Size(520, 48);
             this.panel1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(195, 9);
+            this.label1.Location = new System.Drawing.Point(130, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(352, 55);
             this.label1.TabIndex = 0;
@@ -77,67 +80,74 @@
             this.panel2.Controls.Add(this.kayttajaTunnusTB);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
-            this.panel2.Location = new System.Drawing.Point(12, 326);
+            this.panel2.Location = new System.Drawing.Point(8, 212);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(780, 245);
+            this.panel2.Size = new System.Drawing.Size(520, 159);
             this.panel2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(19, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(238, 37);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Käyttäjätunnus:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 110);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(160, 37);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Salasana:";
-            // 
-            // kayttajaTunnusTB
-            // 
-            this.kayttajaTunnusTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.kayttajaTunnusTB.Location = new System.Drawing.Point(263, 22);
-            this.kayttajaTunnusTB.Name = "kayttajaTunnusTB";
-            this.kayttajaTunnusTB.Size = new System.Drawing.Size(493, 35);
-            this.kayttajaTunnusTB.TabIndex = 2;
-            // 
-            // salaSanaTB
-            // 
-            this.salaSanaTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.salaSanaTB.Location = new System.Drawing.Point(263, 113);
-            this.salaSanaTB.Name = "salaSanaTB";
-            this.salaSanaTB.Size = new System.Drawing.Size(493, 35);
-            this.salaSanaTB.TabIndex = 3;
             // 
             // KirjauduBT
             // 
             this.KirjauduBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.KirjauduBT.Location = new System.Drawing.Point(3, 186);
+            this.KirjauduBT.Location = new System.Drawing.Point(2, 121);
+            this.KirjauduBT.Margin = new System.Windows.Forms.Padding(2);
             this.KirjauduBT.Name = "KirjauduBT";
-            this.KirjauduBT.Size = new System.Drawing.Size(753, 56);
+            this.KirjauduBT.Size = new System.Drawing.Size(502, 36);
             this.KirjauduBT.TabIndex = 4;
             this.KirjauduBT.Text = "Kirjaudu";
             this.KirjauduBT.UseVisualStyleBackColor = true;
             this.KirjauduBT.Click += new System.EventHandler(this.KirjauduBT_Click);
             // 
+            // salaSanaTB
+            // 
+            this.salaSanaTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salaSanaTB.Location = new System.Drawing.Point(175, 73);
+            this.salaSanaTB.Margin = new System.Windows.Forms.Padding(2);
+            this.salaSanaTB.Name = "salaSanaTB";
+            this.salaSanaTB.Size = new System.Drawing.Size(330, 35);
+            this.salaSanaTB.TabIndex = 3;
+            // 
+            // kayttajaTunnusTB
+            // 
+            this.kayttajaTunnusTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kayttajaTunnusTB.Location = new System.Drawing.Point(175, 14);
+            this.kayttajaTunnusTB.Margin = new System.Windows.Forms.Padding(2);
+            this.kayttajaTunnusTB.Name = "kayttajaTunnusTB";
+            this.kayttajaTunnusTB.Size = new System.Drawing.Size(330, 35);
+            this.kayttajaTunnusTB.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Salasana:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Käyttäjätunnus:";
+            // 
             // Kirjautumisikkuna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkOrange;
-            this.ClientSize = new System.Drawing.Size(804, 583);
+            this.ClientSize = new System.Drawing.Size(692, 487);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Kirjautumisikkuna";
             this.Text = "Kirjautumisikkuna";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
---
+++ Hotellivarausjarjestelma/Varaustenhallinta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotellivarausjarjestelma
+{
+    public partial class Varaustenhallinta : Form
+    {
+        public Varaustenhallinta()
+        {
+            InitializeComponent();
+        }
+    }
+}
